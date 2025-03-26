c6781c6776f6855e3a94bc9f4ad605c491bf0ce1
DavidAtMollimers <david.gillberg@iths.se>
2025-03-26 13:18:23 +0100

    Add TDD-developed BankAccount

---
+++ xUnitAPIdemo/BusinessLogic/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace xUnitAPIdemo.BusinessLogic
+{
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+
+        public void Deposit(int amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive");
+            }
+
+            Balance += amount;
+        }
+    }
+}
---
+++ xUnitAPIdemo/UnitTests/2 BankAccount/BankTests.cs
@@ -0,0 +1,33 @@
+﻿
+using Xunit;
+using xUnitAPIdemo.BusinessLogic;
+
+namespace xUnitAPIdemo.UnitTests._2_BankAccount
+{
+    public class BankTest
+    {
+        [Fact]
+        public void Deposit_ShouldIncreaseBalance()
+        {
+            // Arrange
+            BankAccount account = new BankAccount();
+
+            // Act
+            account.Deposit(100);
+
+            // assert
+            Assert.Equal(100, account.Balance);
+        }
+
+        [Fact]
+        public void Deposit_ShouldNotIncreaseBalance()
+        {
+            // Arrange
+            BankAccount account = new BankAccount();
+
+            // assert
+            Assert.Throws<ArgumentException>(() => account.Deposit(-2));
+        }
+    }
+}
+
